38a8b21c262457cec3894bb9a5709b6f1056bc8b
Aleksey Vaskin <alzor85@gmail.com>
2023-12-10 12:57:32 +0700

    feat: add modal calendar markup and styles

--- assets/js/activity.js
+++ assets/js/activity.js
@@ -1,19 +1,22 @@
 const activityPick = () => {
     const overlay = document.querySelector('.overlay');
-    const addActivity = document.querySelectorAll('.schedule__add-activity');
+    const scheduleCalendar = document.querySelector('.schedule__calendar');
     const activityModal = document.querySelector('.pick-activity-modal');
     const activityModalClose = document.querySelector('.pick-activity-modal__close');
     const activityModalContinue = document.querySelector('.pick-activity-modal__continue');
 
     const activityPicked = document.querySelector('.activity-picked');
     const activityPickedBack = document.querySelector('.activity-picked__back');
+    const activityPickedCalendar = document.querySelector('.activity-picked__calendar');
+    const calendarModal = document.querySelector('.calendar-modal');
+    const calendarModalClose = document.querySelector('.calendar-modal__close');
 
-    addActivity.forEach(button => {
-        button.addEventListener('click', () => {
+    scheduleCalendar.addEventListener('click', (evt) => {
+        if (evt.target.classList.contains('schedule__add-activity') || evt.target.classList.contains('schedule__add-icon')) {
             activityModal.classList.toggle('pick-activity-modal--show');
             overlay.classList.toggle('overlay--show');
-        });
-    })
+        }
+    });
 
     activityModalClose.addEventListener('click', () => {
         activityModal.classList.toggle('pick-activity-modal--show');
@@ -26,6 +29,14 @@ const activityPick = () => {
 
     activityPickedBack.addEventListener('click', () => {
         activityPicked.classList.remove('activity-picked--show');
+    });
+
+    activityPickedCalendar.addEventListener('click', () => {
+        calendarModal.classList.add('calendar-modal--show');
+    });
+
+    calendarModalClose.addEventListener('click', () => {
+        calendarModal.classList.remove('calendar-modal--show');
     });
 
 };
