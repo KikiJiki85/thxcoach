96eb775504533a1f66ce0b32b02d5d968de41cd2
Aleksey Vaskin <alzor85@gmail.com>
2023-12-11 17:21:45 +0700

    feat: add all mini modals to activity selection

--- assets/js/activity.js
+++ assets/js/activity.js
@@ -10,12 +10,29 @@ const activityPick = () => {
     const activityPickedBack = document.querySelector('.activity-picked__back');
     const activityPickedCalendar = document.querySelector('.activity-picked__calendar');
     const activityPickedDistance = document.querySelector('.activity-picked__distance');
+    const activityPickedPace = document.querySelector('.activity-picked__pace');
+    const activityPickedDuration = document.querySelector('.activity-picked__duration');
+    const activityPickedHeartRate = document.querySelector('.activity-picked__heart-rate');
+    const activityPickedElevation = document.querySelector('.activity-picked__elevation');
 
     const calendarModal = document.querySelector('.calendar-modal');
     const calendarModalClose = document.querySelector('.calendar-modal__close');
 
     const distanceModal = document.querySelector('.distance-modal');
     const distanceModalClose = document.querySelector('.distance-modal__close');
+
+    const paceModal = document.querySelector('.pace-modal');
+    const paceModalClose = document.querySelector('.pace-modal__close');
+
+    const durationModal = document.querySelector('.duration-modal');
+    const durationModalClose = document.querySelector('.duration-modal__close');
+
+    const heartRateModal = document.querySelector('.heart-rate-modal');
+    const heartRateModalClose = document.querySelector('.heart-rate-modal__close');
+
+    const elevationModal = document.querySelector('.elevation-modal');
+    const elevationModalClose = document.querySelector('.elevation-modal__close');
+
 
     scheduleCalendar.addEventListener('click', (evt) => {
         if (evt.target.classList.contains('schedule__add-activity') || evt.target.classList.contains('schedule__add-icon')) {
@@ -42,20 +59,58 @@ const activityPick = () => {
         overlay2.classList.add('overlay2--show');
     });
 
+    calendarModalClose.addEventListener('click', () => {
+        calendarModal.classList.remove('calendar-modal--show');
+        overlay2.classList.remove('overlay2--show');
+    });
+
     activityPickedDistance.addEventListener('click', () => {
-        distanceModal.classList.add('distance-modal--show');
+        distanceModal.classList.add('mini-modal--show');
         overlay2.classList.add('overlay2--show');
     });
 
     distanceModalClose.addEventListener('click', () => {
-        distanceModal.classList.remove('distance-modal--show');
+        distanceModal.classList.remove('mini-modal--show');
         overlay2.classList.remove('overlay2--show');
     });
 
+    activityPickedPace.addEventListener('click', () => {
+        paceModal.classList.add('mini-modal--show');
+        overlay2.classList.add('overlay2--show');
+    });
 
+    paceModalClose.addEventListener('click', () => {
+        paceModal.classList.remove('mini-modal--show');
+        overlay2.classList.remove('overlay2--show');
+    });
 
-    calendarModalClose.addEventListener('click', () => {
-        calendarModal.classList.remove('calendar-modal--show');
+    activityPickedDuration.addEventListener('click', () => {
+        durationModal.classList.add('mini-modal--show');
+        overlay2.classList.add('overlay2--show');
+    });
+
+    durationModalClose.addEventListener('click', () => {
+        durationModal.classList.remove('mini-modal--show');
+        overlay2.classList.remove('overlay2--show');
+    });
+
+    activityPickedHeartRate.addEventListener('click', () => {
+        heartRateModal.classList.add('mini-modal--show');
+        overlay2.classList.add('overlay2--show');
+    });
+
+    heartRateModalClose.addEventListener('click', () => {
+        heartRateModal.classList.remove('mini-modal--show');
+        overlay2.classList.remove('overlay2--show');
+    });
+
+    activityPickedElevation.addEventListener('click', () => {
+        elevationModal.classList.add('mini-modal--show');
+        overlay2.classList.add('overlay2--show');
+    });
+
+    elevationModalClose.addEventListener('click', () => {
+        elevationModal.classList.remove('mini-modal--show');
         overlay2.classList.remove('overlay2--show');
     });
 
